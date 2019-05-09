9b7c524aa804e0425c0af61f68027fc4dc641c79
carlojohn98 <johncarlomiel@gmail.com>
2019-05-09 19:10:34 +0800

    Update

--- src/app/configs/config.ts
+++ src/app/configs/config.ts
@@ -1,11 +1,11 @@
 
-//For testing purposes
+// //For testing purposes
 export const config = {
     ip: "http://localhost:8080/"
 }
 
 
-//For deployment
+// For deployment
 // export const config = {
 //     ip: "https://gordoncollegesds.herokuapp.com/"
 // }
